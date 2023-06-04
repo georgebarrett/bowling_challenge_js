1ef376441b764b713fb3fc14c0d326d4a7337628
George Barrett <georgeaustinbarrett@gmail.com>
2023-06-04 14:45:57 +0100

    test for a scenario where a player scores 21 after three frames. the player achieves no strike or spare bonuses during these frames. running GREEN

--- scorecard.test.js
+++ scorecard.test.js
@@ -24,5 +24,15 @@ describe('Scorecard', () => {
 
         expect(scorecard.getFrames()).toEqual([[2, 3, 0]]);
         expect(scorecard.getTotalScore()).toBe(5);  
+    });
+
+    it('should return the score of 21 after the third frame', () => {
+        scorecard.addFrame(4, 4, 0);
+        scorecard.addFrame(4, 4, 0);
+        scorecard.addFrame(4, 1, 0);
+        calculateScore();
+
+        expect(scorecard.getFrames()).toEqual([[4, 4, 0], [4, 4, 0], [4, 1, 0]]);
+        expect(scorecard.getTotalScore()).toBe(21);  
     })
 })
